8c2055cef5170c463dbeff6e9a9c7857a874f74b
Jonathan <jonchengc3@gmail.com>
2017-12-20 10:25:30 -0500

    works
    - tested via JUnit and Maven
    - Green bar // Build Success

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -6,17 +6,22 @@ public class ReverseString {
 	
     public String reverse(String input) {
     	
+    	// local variables to break string apart
     	String[] splitLetters = input.split("(?!^)");
     	int iterLimit = splitLetters.length;
-    	String finalizedReverse = null;
+    	String finalizedReverse = "";
     	
-    	
+    	// reverse for-loop (minus 1 b/c start-from-zero convention)
     	for (int itr = iterLimit-1; itr >= 0; itr--) {
-    		System.out.println("Currently on iteration:" + iterLimit);
+    		//System.out.println("Currently on iteration:" + itr);
     		finalizedReverse += splitLetters[itr];
     	}
     	
+    	// see if string is actually reversed
+    	//System.out.println("final reversed word is |"+finalizedReverse+"|" );
     	return finalizedReverse;
+    	
+    	// old return statement; not allowed to use for challenge
         //return new StringBuffer(input).reverse().toString();
     }
     
