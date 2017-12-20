c41251202d1b0e045c30f25965cb547a0f281996
Jonathan <jonchengc3@gmail.com>
2017-12-20 10:10:34 -0500

    Wrote reverse string method
    - Reverse string method without using Java's StringBuffer reverse method

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -1,7 +1,23 @@
 package quiz;
 
+import java.util.ArrayList;
+
 public class ReverseString {
+	
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+    	
+    	String[] splitLetters = input.split("(?!^)");
+    	int iterLimit = splitLetters.length;
+    	String finalizedReverse = null;
+    	
+    	
+    	for (int itr = iterLimit-1; itr >= 0; itr--) {
+    		System.out.println("Currently on iteration:" + iterLimit);
+    		finalizedReverse += splitLetters[itr];
+    	}
+    	
+    	return finalizedReverse;
+        //return new StringBuffer(input).reverse().toString();
     }
+    
 }
